e87f9c38ec8111dbbeb5e635447944c2b498d5d1
heric <hericpeng@gmail.com>
2017-03-20 16:33:50 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -44,6 +44,29 @@ class GroupsController < ApplicationController
       redirect_to groups_path
     end
 
+    def join
+      @group = Group.find(params[:id])
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice]="加入本讨论版成功"
+      else
+        flash[:warning]="你已经是讨论版成员了"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert]="已退出讨论版"
+      else
+        flash[:warning]="你不是本讨论版成员，怎么退出，xd"
+      end
+      redirect_to group_path(@group)
+    end
+
 
   private
 
@@ -53,8 +76,9 @@ class GroupsController < ApplicationController
     if current_user != @group.user
       redirect_to root_path, alert:"You have no permiision."
     end
-
   end
+
+
 
   def group_params
     params.require(:group).permit(:title,:description)
