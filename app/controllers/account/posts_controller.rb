15bf7d2d42b3de32ba46f1e0cfad8d4135849025
heric <hericpeng@gmail.com>
2017-03-20 20:42:45 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,7 +1,39 @@
 class Account::PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only:[:edit, :update]
 
   def index
     @posts = current_user.posts
   end
+  def edit
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      if current_user != @post.user
+        redirect_to account_posts_path, alert:"You have no permission."
+      end
+  end
+
+  def update
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+          redirect_to account_posts_path, notice: "Post Update Success"
+      else
+          render :edit
+      end
+   end
+
+   def destroy
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+     if @post.destroy
+       redirect_to account_posts_path, alert: "Post Deleted"
+     end
+   end
+
+   private
+
+   def post_params
+       params.require(:post).permit(:content)
+   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,53 @@
 class PostsController < ApplicationController
-  def new
-    @group = Group.find(params[:group_id])
-    @post = Post.new
-  end
-
-  def create
-    @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
-    @post.group = @group
-    @post.user = current_user
-
-    if @post.save
-      redirect_to group_path(@group)
-    else
-      render :new
+  before_action :authenticate_user!, only:[:edit, :update,:destroy]
+    def new
+        @group = Group.find(params[:group_id])
+        @post = Post.new
     end
-  end
 
-  private
+    def create
+        @group = Group.find(params[:group_id])
+        @post = Post.new(post_params)
+        @post.group = @group
+        @post.user = current_user
 
-  def post_params
-    params.require(:post).permit(:content)
-  end
-  
+        if @post.save
+            redirect_to group_path(@group)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+        if current_user != @post.user
+          redirect_to account_posts_path, alert:"You have no permission."
+        end
+    end
+
+    def update
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to account_posts_path, notice: "Post Update Success"
+        else
+            render :edit
+        end
+     end
+
+     def destroy
+       @group = Group.find(params[:group_id])
+       @post = Post.find(params[:id])
+       if @post.destroy
+         redirect_to account_posts_path, alert: "Post Deleted"
+       end
+     end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content)
+    end
 end
